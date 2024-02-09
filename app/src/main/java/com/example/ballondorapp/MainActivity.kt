14f0e8c02e94a09ce89bbe6669ad651893141266
Daarkooo <hichemhpro@gmail.com>
2024-02-09 02:30:10 +0100

    added dark theme & shape & typography & top bar

--- app/src/main/java/com/example/ballondorapp/MainActivity.kt
+++ app/src/main/java/com/example/ballondorapp/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.ballondorapp
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ballondorapp.data.Player
+import com.example.ballondorapp.data.players
 import com.example.compose.BallonDorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +31,7 @@ class MainActivity : ComponentActivity() {
             BallonDorAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    BallonDorApp()
                 }
             }
         }
@@ -27,9 +39,96 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun BallonDorApp(){
+    Scaffold(
+        topBar = {
+            BallonDorAppBar()
+        }
+    ){
+        it ->
+        LazyColumn(contentPadding = it){
+            items(players){
+                ListItem(player = it,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small)))
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(
+    player: Player,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = modifier){
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            PlayerIcon(player.imageResourceId)
+            PlayerInformation(player.name, player.nationality)
+        }
+    }
+}
+
+@Composable
+fun PlayerIcon(
+    @DrawableRes playerIcon: Int,
+    modifier: Modifier = Modifier
+){
+   Image(
+       modifier = modifier
+           .size(dimensionResource(R.dimen.image_size))
+           .padding(dimensionResource(R.dimen.padding_small))
+           .clip(MaterialTheme.shapes.small),
+       contentScale = ContentScale.FillHeight,
+       painter = painterResource(playerIcon),
+       contentDescription = null
+   )
+
+}
+
+@Composable
+fun PlayerInformation(
+    @StringRes playerName: Int,
+    playerNationality: String,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier){
+        Text(
+            text = stringResource(playerName),
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+        )
+        Text(
+            text = stringResource(R.string.nationality ,playerNationality),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BallonDorAppBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.logo_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ballon_dor_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
         modifier = modifier
     )
 }
@@ -37,8 +136,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true,
     showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    BallonDorAppTheme {
-        Greeting("Android")
+fun BallonDorPreview() {
+    BallonDorAppTheme(darkTheme = false) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            BallonDorApp()
+        }
     }
 }
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun BallonDorDarkThemPreview() {
+    BallonDorAppTheme(darkTheme = true) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            BallonDorApp()
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/ballondorapp/data/Player.kt
@@ -0,0 +1,25 @@
+package com.example.ballondorapp.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.ballondorapp.R
+
+data class Player(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val nationality: String,
+    @StringRes val years: Int
+)
+
+val players = listOf(
+    Player(R.drawable.messi, R.string.messi,"Argentina", R.string.messi_description),
+    Player(R.drawable.cr7, R.string.cr7,"Portugal" ,R.string.cr7_description),
+    Player(R.drawable.platini, R.string.platini,"France" ,R.string.platini_description),
+    Player(R.drawable.cruyff, R.string.cruyff,"Netherlands" ,R.string.cruyff_description),
+    Player(R.drawable.vanbasten, R.string.vanBasten,"Netherlands", R.string.vanBasten_description),
+    Player(R.drawable.beckenbauer, R.string.beckenbauer,"Germany", R.string.beckenbauer_description),
+    Player(R.drawable.ronaldo, R.string.ronaldo,"Brazil" ,R.string.ronaldo_description),
+    Player(R.drawable.ronaldinho, R.string.ronaldinho,"Brazil", R.string.ronaldinho_description),
+    Player(R.drawable.zidane, R.string.zidane,"France" , R.string.zidane_description),
+    Player(R.drawable.kaka, R.string.kaka, "Brazil",R.string.kaka_description),
+)
